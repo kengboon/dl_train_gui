809a451adb7a49e454f60a1d5c4d24eb6c1204a5
KB Teo <kengboon@users.noreply.github.com>
2022-11-24 16:10:59 +0800

    Update build with icon

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ call conda activate eel
 :: Build the distributable binary
 :: See PyInstaller documentation for optional argurments
 :: Include modules required in --hidden-import
-python -m eel run.py "dl_train_gui" --onefile --noconsole --name "dl_train_gui" --hidden-import eel
+python -m eel run.py "dl_train_gui" --onefile --noconsole --name "dl_train_gui" --icon "dl_train_gui/web/img/neural_net.ico" --hidden-import eel
 
+:: Optional: pause
 pause
