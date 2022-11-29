ad6b93e082bc2a885d9b251a63a20583ab820c56
KB Teo <kengboon@users.noreply.github.com>
2022-11-29 11:50:14 +0800

    Include tkinter

--- build.bat
+++ build.bat
@@ -4,7 +4,10 @@ call conda activate eel
 :: Build the distributable binary
 :: See PyInstaller documentation for optional argurments
 :: Include modules required in --hidden-import
-python -m eel run.py "dl_train_gui" --onefile --noconsole --name "dl_train_gui" --icon "dl_train_gui/web/img/neural_net.ico" --hidden-import eel
+python -m eel run.py "dl_train_gui" --onefile --noconsole --name "dl_train_gui" --icon "dl_train_gui/web/img/neural_net.ico"^
+ --hidden-import eel^
+ --hidden-import tkinter^
+ --hidden-import tkinter.filedialog
 
 :: Optional: pause
 pause
